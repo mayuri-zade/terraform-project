bf2b70643a4eb8285669b6c0dc817c5797e93aa3
Mayurizade <100784756+mayuri-zade@users.noreply.github.com>
2024-04-01 21:57:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ region = "ap-southeast-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-009e46eef82e25fef"
   instance_type   = "t2.micro"
   key_name        = "rahamabc"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-south-1"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
