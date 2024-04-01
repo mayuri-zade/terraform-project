e7dcd51dc94c8cbf784c68bd1ff449b9a69e5c33
Mayurizade <100784756+mayuri-zade@users.noreply.github.com>
2024-04-01 22:09:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "one" {
   instance_type   = "t2.micro"
   key_name        = "rahamabc"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap "
+  availability_zone = "ap-southeast-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
